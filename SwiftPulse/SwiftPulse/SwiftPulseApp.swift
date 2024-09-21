ab057dc8f59cb3ef12869c285183c49e1612fd85
Deep Bhupatkar <bhupatkardeep@gmail.com>
2024-09-21 11:14:37 +0530

    Initial Commit

---
+++ SwiftPulse/SwiftPulse/SwiftPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPulseApp.swift
+//  SwiftPulse
+//
+//  Created by Deep Bhupatkar on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
